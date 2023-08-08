91425fbf044048a5e94478c83342f52fb2a41f2a
katelandcastelin <kateland.cas@gmail.com>
2023-08-08 14:30:44 +0200

    feat/order-page: added media

--- app/orderProducts-data/orderProducts.js
+++ app/orderProducts-data/orderProducts.js
@@ -13,33 +13,45 @@ const orderProducts = [
   },
   {
     id: 3,
-    image: 'image_url_3',
+    image: '/images/order-products/three-graces.jpg',
     price: '$15',
     description: 'Product 3',
   },
   {
     id: 4,
-    image: 'image_url_4',
+    image: '/images/order-products/cat-1.jpg',
     price: '$15',
     description: 'Product 4',
   },
   {
     id: 5,
-    image: 'image_url_5',
+    image: '/images/order-products/ub-sn.jpg',
     price: '$15',
     description: 'Product 5',
   },
   {
     id: 6,
-    image: 'image_url_6',
+    image: '/images/order-products/tapestry.jpeg',
     price: '$15',
     description: 'Product 6',
   },
   {
     id: 7,
-    image: 'image_url_7',
+    image: '/images/order-products/ub-chain.jpg',
     price: '$15',
     description: 'Product 7',
+  },
+  {
+    id: 8,
+    image: '/images/order-products/fb-bov.jpg',
+    price: '$15',
+    description: 'Product 8',
+  },
+  {
+    id: 9,
+    image: '/images/order-products/coa.png',
+    price: '$15',
+    description: 'Product 9',
   },
 ];
 
