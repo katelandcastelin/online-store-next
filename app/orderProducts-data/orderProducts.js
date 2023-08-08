1eca9d5ff3da4e318a50729cfa51feab29e45086
katelandcastelin <kateland.cas@gmail.com>
2023-08-08 09:29:36 +0200

    feat/order-page: created order page and added styles

---
+++ app/orderProducts-data/orderProducts.js
@@ -0,0 +1,46 @@
+const orderProducts = [
+  {
+    id: 1,
+    image: 'image_url_1',
+    price: '$10',
+    description: 'Product 1',
+  },
+  {
+    id: 2,
+    image: 'image_url_2',
+    price: '$15',
+    description: 'Product 2',
+  },
+  {
+    id: 3,
+    image: 'image_url_3',
+    price: '$15',
+    description: 'Product 3',
+  },
+  {
+    id: 4,
+    image: 'image_url_4',
+    price: '$15',
+    description: 'Product 4',
+  },
+  {
+    id: 5,
+    image: 'image_url_5',
+    price: '$15',
+    description: 'Product 5',
+  },
+  {
+    id: 6,
+    image: 'image_url_6',
+    price: '$15',
+    description: 'Product 6',
+  },
+  {
+    id: 7,
+    image: 'image_url_7',
+    price: '$15',
+    description: 'Product 7',
+  },
+];
+
+export default orderProducts;
