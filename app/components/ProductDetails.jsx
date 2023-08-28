5b5010d3fabdeb6d6e68c411681022099b6e51cc
katelandcastelin <kateland.cas@gmail.com>
2023-08-28 19:14:41 +0200

    feat/product-details-page: added mui and accordion component

--- app/components/ProductDetails.jsx
+++ app/components/ProductDetails.jsx
@@ -1,22 +1,93 @@
 'use client';
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
+import Accordion from './Accordion';
 
 const Container = styled.div`
+  height: 100vh;
+  margin-top: 5%;
+  display: flex;
+  justify-content: center;
+`;
+
+const ImageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 90%;
+  width: 50%;
+  margin-bottom: 10px;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
+const ProductInfoContainer = styled.div`
+  height: 90%;
+  width: 30%;
+  margin-left: 3%;
+`;
+
+const Description = styled.p`
+  font-size: xx-large;
+`;
+
+const Price = styled.p`
+  font-size: larger;
+`;
+
+const DetailsAndCareContainer = styled.div`
+  width: 100%;
   border: 1px solid #000;
-  height: 400px;
-  width: 400px;
+`;
+
+const SizeCartContainer = styled.div`
+  width: 100%;
+  border: 1px solid #000;
+`;
+
+const ShippingInfoContainer = styled.div`
+  width: 100%;
+  border: 1px solid #000;
+`;
+
+const ReviewsContainer = styled.div`
+  width: 100%;
+  border: 1px solid #000;
 `;
 
 export default function ProductDetails({ product }) {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [product]);
+
   return (
-    <div>
-      <div>
-        <img />
-      </div>
-      <Container>Hello</Container>
-      <p>R {product.price}</p>
-      <p>{product.description}</p>
-    </div>
+    <Container>
+      <ImageContainer>
+        <img src={product.image} />
+      </ImageContainer>
+      <ProductInfoContainer>
+        <Description>{product.description}</Description>
+        <br />
+        <Price>R {product.price}</Price>
+        <br />
+        <Accordion />
+
+        {/* <DetailsAndCareContainer>
+          Details and Care
+        </DetailsAndCareContainer>
+        <SizeCartContainer>
+          Size Chart
+        </SizeCartContainer>
+        <ShippingInfoContainer>
+          Shipping Information
+        </ShippingInfoContainer>
+        <ReviewsContainer>
+          Reviews
+        </ReviewsContainer> */}
+      </ProductInfoContainer>
+    </Container>
   )
 }
