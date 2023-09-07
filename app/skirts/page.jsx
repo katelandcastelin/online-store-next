3036e1222353b1ea5593630006ab0607cb3b4a42
katelandcastelin <kateland.cas@gmail.com>
2023-09-07 14:04:37 +0200

    feat/skirts-page: added skirt product data, grid, and map

--- app/skirts/page.jsx
+++ app/skirts/page.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import Link from 'next/link';
 import styles from './skirtsPage.module.css';
 import corsetProducts from '../corsetsProducts-data/corsetsProducts';
+import skirtProducts from './skirtsProducts-data/skirtsProducts';
 
 export default function SkirtsPage() {
   return (
     <div className={styles.main}>
       <h1>Shop Skirts</h1>
       <h2>Coming Soon..</h2>
-
-      {/* <div className={styles.grid}>
-        {corsetProducts.map((item) => (
-          <Link key={item.id} href={`/corsets/${item.id}`} className={styles.card}>
+      <div className={styles.grid}>
+        {skirtProducts.map((item) => (
+          <Link key={item.id} href={`/skirts/${item.id}`} className={styles.card}>
             <div>
-              <div className={styles.corsetImage}>
+              <div className={styles.skirtImage}>
                 <img src={item.image} alt={item.description} />
               </div>
               <p>R {item.price}</p>
@@ -21,7 +21,7 @@ export default function SkirtsPage() {
             </div>
           </Link>
         ))}
-      </div> */}
+      </div>
     </div>
   )
 }
---
+++ app/skirts/skirtsProducts-data/skirtsProducts.js
@@ -0,0 +1,16 @@
+const skirtProducts = [
+  {
+    id: 1,
+    image: '/images/skirt-products/skirt.jpeg',
+    price: 880,
+    description: 'Black brocade',
+  },
+  {
+    id: 2,
+    image: '/images/skirt-products/skirt.jpeg',
+    price: 880,
+    description: 'Birth of Venus',
+  }
+];
+
+export default skirtProducts;
