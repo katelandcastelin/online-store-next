5ff0001f3818c0ce99c19ad4e495bc7d1ee52248
katelandcastelin <kateland.cas@gmail.com>
2023-09-15 13:46:59 +0200

    feat/log-in-page: added layout and styling to forms and page

--- app/log-in/page.jsx
+++ app/log-in/page.jsx
@@ -3,12 +3,16 @@ import React from 'react';
 import styles from './logInPage.module.css';
 import styled from 'styled-components';
 
+const Label = styled.label`
+  margin-bottom: 10px;
+`;
+
 const Input = styled.input`
   background-color: #fff;
   border: none;
   border-radius: 5px;
-  padding: 10px;
-  margin: 10px;
+  padding: 15px;
+  margin-bottom: 20px;
 `;
 
 const SubmitButton = styled.input`
@@ -23,20 +27,28 @@ export default function LogInPage() {
       <div className={styles.formsContainer}>
         <div className={styles.logIn}>
           <form className={styles.form}>
-            Log in
-            <label>Username</label>
+            <p className={styles.formHeader}>Log in</p>
+            <Label>Username</Label>
             <Input type="username" placeholder="Enter Username" name="uname" required />
-            <label>Password</label>
+            <Label>Password</Label>
             <Input type="password" placeholder="Enter Password" name="pwd" required />
-            {/* <SubmitButton type="submit" value="Log in" /> */}
           </form>
           <SubmitButton type="submit" value="Log in" />
         </div>
 
         <div className={styles.signUp}>
           <form className={styles.form}>
-            Sign me up
+            <p className={styles.formHeader}>Sign me up</p>
+            <Label>Username</Label>
+            <Input type="username" placeholder="Enter Username" name="uname" required />
+            <Label>Email</Label>
+            <Input type="email" placeholder="Enter email" name="email" required />
+            <Label>Password</Label>
+            <Input type="password" placeholder="Enter Password" name="pwd" required />
+            <Label>Confirm password</Label>
+            <Input type="confirm password" placeholder="Confirm Password" name="pwd" required />
           </form>
+          <SubmitButton type="submit" value="Sign up" />
         </div>
       </div>
     </div>
