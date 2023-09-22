40aa287e824cd12779b5b2f56af1006343d36c28
katelandcastelin <kateland.cas@gmail.com>
2023-09-22 12:17:42 +0200

    feat/skirts-product-page: created product details page for skirts

---
+++ app/skirts/[id]/page.jsx
@@ -0,0 +1,14 @@
+'use client';
+import React from 'react';
+import ProductDetails from '../../components/ProductDetails';
+import skirtsProducts from '../skirtsProducts-data/skirtsProducts';
+
+export default function SkirtDetailPage({ params }) {
+  let product = skirtsProducts.find((entry) => entry.id === Number(params.id));
+
+  return (
+    <div>
+      <ProductDetails product={product} />
+    </div>
+  )
+}
