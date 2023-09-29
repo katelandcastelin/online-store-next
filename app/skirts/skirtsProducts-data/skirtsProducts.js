8c5c24a5afffeb9408b1f36cf3ad2c4c2bff83d2
katelandcastelin <kateland.cas@gmail.com>
2023-09-29 16:43:42 +0200

    feat/skirt-gallery-data: creating gallery file for skirt product details page

--- app/skirts/skirtsProducts-data/skirtsProducts.js
+++ app/skirts/skirtsProducts-data/skirtsProducts.js
@@ -11,30 +11,6 @@ const skirtProducts = [
     price: 880,
     description: 'Black brocade',
   },
-  // {
-  //   id: 3,
-  //   image: '/images/skirt-products/skirt.jpeg',
-  //   price: 880,
-  //   description: 'Black brocade',
-  // },
-  // {
-  //   id: 4,
-  //   image: '/images/skirt-products/skirt.jpeg',
-  //   price: 880,
-  //   description: 'Birth of Venus',
-  // },
-  // {
-  //   id: 5,
-  //   image: '/images/skirt-products/skirt.jpeg',
-  //   price: 880,
-  //   description: 'Black brocade',
-  // },
-  // {
-  //   id: 6,
-  //   image: '/images/skirt-products/skirt.jpeg',
-  //   price: 880,
-  //   description: 'Birth of Venus',
-  // }
 ];
 
 export default skirtProducts;
